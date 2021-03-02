5aa1ddc5c4c9095b7813a79084f4fff626c7efe1
ndev <napodevesa@gmail.com>
2021-03-02 10:37:32 -0300

    3.2

---
+++ 3.r
@@ -0,0 +1,35 @@
+# Create a function pow_two()
+
+pow_two <- function (x){
+  
+  x^2
+}
+
+# Use the function
+pow_two (12)
+
+# Create a function sum_abs()
+
+sum_abs <- function (a,b)
+{
+  abs(a) + abs(b)
+  
+}
+
+# Use the function
+
+ sum_abs(-2,3)
+
+
+# Define the function hello()
+
+hello <- function(){
+  print ("Hi there!" )
+  TRUE
+}
+
+# Call the function hello()
+
+hello ()
+
+
