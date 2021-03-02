5fc4536a821ca3be95c85620b40314ac5586035a
ndev <napodevesa@gmail.com>
2021-03-02 09:23:25 -0300

    2.3

--- 2.r
+++ 2.r
@@ -77,3 +77,10 @@ mean (linkedin)
 
 # Advanced average of linkedin
 mean (linkedin, na.rm=TRUE)
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, NA, 17, 14)
+facebook <- c(17, NA, 5, 16, 8, 13, 14)
+
+# Calculate the mean absolute deviation
+mean(abs(linkedin - facebook), na.rm = TRUE)
